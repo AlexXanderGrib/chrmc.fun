086b74a188613961bc60ab2796135dc04132cc1b
Alexander Gribochkin <me@alexxgrib.me>
2022-02-28 22:09:59 +0300

    chore: picked up trash

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps: [
+    {
+      name: "chrmc",
+      script: "npm",
+      args: "start",
+      instances: 2,
+      exec_mode: "cluster",
+      min_uptime: "10s",
+      listen_timeout: 8000,
+      max_memory_restart: "1G",
+      env: {
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: "production"
+      },
+      env_development: {
+        NODE_ENV: "development"
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: "www",
+      host: "37.77.106.193",
+      ref: "origin/main",
+      repo: "git@github.com:AlexXanderGrib/chrmc.fun.git",
+      path: "/home/www/app",
+      "post-deploy": "npm ci --prod",
+      key: "./key.pem"
+    }
+  }
+};
